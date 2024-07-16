629673d226d4e0cae814648f2d01a4aed0b1b6b8
MilosD13 <milosdokic13@gmail.com>
2024-07-16 15:24:24 +1000

    Add auth to swagger for easier testing

--- TodoApi/StartupConfig/DependencyInjectionsExtensions.cs
+++ TodoApi/StartupConfig/DependencyInjectionsExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using TodoLibrary.DataAccess;
 
@@ -11,8 +12,42 @@ public static class DependencyInjectionsExtensions
     {
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.AddSwaggerServices();
     }
+
+    public static void AddSwaggerServices(this WebApplicationBuilder builder)
+    {
+        var securityScheme = new OpenApiSecurityScheme()
+        {
+            Name = "Authorisatio",
+            Description = "JWT Autorisation header info using bearer tokens",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        };
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "bearerAuth"
+                    }
+                },
+                new string[]{}
+            }
+        };
+        builder.Services.AddSwaggerGen(opts =>
+        {
+            opts.AddSecurityDefinition("bearerAuth", securityScheme);
+            opts.AddSecurityRequirement(securityRequirement);
+        } );
+
+    }
+
 
     public static void AddCustomServices(this WebApplicationBuilder builder)
     {
